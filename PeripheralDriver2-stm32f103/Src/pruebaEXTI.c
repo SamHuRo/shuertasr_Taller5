8af0c86da1b092590de3eb818d4d5aa31ca5aa39
Samuel Huertas <samhuro3@gmail.com>
2023-05-12 22:22:22 -0500

    Creacion de pruebas

---
+++ PeripheralDriver2-stm32f103/Src/pruebaEXTI.c
@@ -0,0 +1,8 @@
+/*
+ * pruebaEXTI.c
+ *
+ *  Created on: May 12, 2023
+ *      Author: samuel
+ */
+
+
---
+++ PeripheralDriver2-stm32f103/Src/pruebaUSART.c
@@ -0,0 +1,8 @@
+/*
+ * pruebaUSART.c
+ *
+ *  Created on: May 12, 2023
+ *      Author: samuel
+ */
+
+
