b6cc0b0b580d5dae3d8b78064ce4f897a9f96314
Samuel Huertas <samhuro3@gmail.com>
2023-06-04 15:16:42 -0500

    Actualizacion solucionExamen.c

--- solucionExamen/App/Src/solucionExamen.c
+++ solucionExamen/App/Src/solucionExamen.c
@@ -4,5 +4,154 @@
  *  Created on: Jun 2, 2023
  *      Author: samuel
  */
+#include <stm32f4xx.h>
+
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "AdcDriver.h"
+#include "BasicTimer.h"
+#include "ExtiDriver.h"
+#include "GPIOxDriver.h"
+#include "I2CDriver.h"
+#include "MPUxDriver.h"
+#include "PLLDriver.h"
+#include "PwmDriver.h"
+#include "SysTickDriver.h"
+#include "USARTxDriver.h"
+
+/*==========================
+ *Configuracion del BlinkyPin
+ *==========================*/
+GPIO_Handler_t BlinkyPin = {0};
+BasicTimer_Handler_t handlerTimerBlinkyPin = {0};
+
+/*==============================
+ * Configuracion del USART
+ *===============================*/
+GPIO_Handler_t handlerPinTX = {0};
+GPIO_Handler_t handlerPinRX = {0};
+USART_Handler_t handlerTerminal = {0};
+
+/*==========================
+ * Configuracion para el I2C
+ * ==========================*/
+//Acelerometro
+GPIO_Handler_t SDAAccel = {0};
+GPIO_Handler_t SCLAccel = {0};
+I2C_Handler_t handlerAccelerometer = {0};
+
+/*=========================
+ * Cabeceras de las funciones
+ * ==========================*/
+void initSystem(void);
+
+/*=========================
+ * 	 Variables Globales
+ * ==========================*/
+uint8_t rxData = 0;
+uint16_t contadorRecepcion = 0;
+uint8_t bufferReception[];
+bool stringComplete = false;
+unsigned int firstParameter;
+unsigned int secondParameter;
+
+int main(void){
+
+}
 
 
+
+/*=================================================
+ * Funcion para cargar la configuracion de los pines
+ * ================================================*/
+void initSystem(void){
+	/*========================================
+	 * Configuracion del Blinky Pin
+	 *========================================*/
+	BlinkyPin.pGPIOx											= GPIOH;
+	BlinkyPin.GPIO_PinConfig.GPIO_PinNumber						= PIN_1;
+	BlinkyPin.GPIO_PinConfig.GPIO_PinMode						= GPIO_MODE_OUT;
+	BlinkyPin.GPIO_PinConfig.GPIO_PinOPType						= GPIO_OTYPER_PUSHPULL;
+	BlinkyPin.GPIO_PinConfig.GPIO_PinSpeed						= GPIO_OSPEED_HIGH;
+	//Cargamos la configuracion
+	GPIO_Config(&BlinkyPin);
+	/* Configuracion del timer */
+	//Seleccionar el timer que se va a usar
+	handlerTimerBlinkyPin.ptrTIMx								= TIM2;
+	//Configuracion con la cual se maneja el timer
+	handlerTimerBlinkyPin.TIMx_Config.TIMx_mode					= BTIMER_MODE_UP;
+	handlerTimerBlinkyPin.TIMx_Config.TIMx_period				= 2500;
+	handlerTimerBlinkyPin.TIMx_Config.TIMx_speed				= BTIMER_SPEED_100us_80MHz;
+	handlerTimerBlinkyPin.TIMx_Config.TIMx_interruptEnable		= SET;
+	//Cargar la configuracion del Timer
+	BasicTimer_Config(&handlerTimerBlinkyPin);
+
+	/*========================================
+	 * Configuracion de la comunicacion serial
+	 *========================================*/
+	//Configuracion para el pin de transmicion
+	handlerPinTX.pGPIOx 										= GPIOC;
+	handlerPinTX.GPIO_PinConfig.GPIO_PinNumber					= PIN_6;
+	handlerPinTX.GPIO_PinConfig.GPIO_PinMode					= GPIO_MODE_ALTFN;
+	handlerPinTX.GPIO_PinConfig.GPIO_PinOPType					= GPIO_OTYPER_PUSHPULL;
+	handlerPinTX.GPIO_PinConfig.GPIO_PinPuPdControl				= GPIO_PUPDR_NOTHING;
+	handlerPinTX.GPIO_PinConfig.GPIO_PinSpeed					= GPIO_OSPEED_FAST;
+	handlerPinTX.GPIO_PinConfig.GPIO_PinAltFunMode				= AF8;
+	//Cargar la configuracion del pin
+	GPIO_Config(&handlerPinTX);
+	//configuracion del pin para la recepcion
+	handlerPinRX.pGPIOx											= GPIOC;
+	handlerPinRX.GPIO_PinConfig.GPIO_PinNumber					= PIN_7;
+	handlerPinRX.GPIO_PinConfig.GPIO_PinMode					= GPIO_MODE_ALTFN;
+	handlerPinRX.GPIO_PinConfig.GPIO_PinOPType					= GPIO_OTYPER_PUSHPULL;
+	handlerPinRX.GPIO_PinConfig.GPIO_PinPuPdControl				= GPIO_PUPDR_NOTHING;
+	handlerPinRX.GPIO_PinConfig.GPIO_PinSpeed					= GPIO_OSPEED_FAST;
+	handlerPinRX.GPIO_PinConfig.GPIO_PinAltFunMode				= AF8;
+	//Cargar la configuracion del pin
+	GPIO_Config(&handlerPinRX);
+	//Configuracion del USART
+	handlerTerminal.ptrUSARTx 									= USART2;
+	handlerTerminal.USART_Config.USART_baudrate 				= USART_BAUDRATE_115200;
+	handlerTerminal.USART_Config.USART_datasize					= USART_DATASIZE_8BIT;
+	handlerTerminal.USART_Config.USART_parity					= USART_PARITY_NONE;
+	handlerTerminal.USART_Config.USART_stopbits					= USART_STOPBIT_1;
+	handlerTerminal.USART_Config.USART_mode						= USART_MODE_RXTX;
+	handlerTerminal.USART_Config.USART_enableIntRX				= USART_RX_INTERRUP_ENABLE;
+	handlerTerminal.USART_Config.USART_enableIntTX				= USART_TX_INTERRUP_ENABLE;
+	handlerTerminal.USART_Config.USART_PLL_Enable				= PLL_ENABLE;
+	//Cargar la configuracion del USART
+	USART_Config(&handlerTerminal);
+
+	/*=========================================================
+	 * Configuracion para el protocolo I2C para el Acelerometro
+	 *==========================================================*/
+	//Configuracion de los pines para el I2C -> SCL
+	SCLAccel.pGPIOx												= GPIOB;
+	SCLAccel.GPIO_PinConfig.GPIO_PinNumber						= PIN_8;
+	SCLAccel.GPIO_PinConfig.GPIO_PinMode						= GPIO_MODE_ALTFN;
+	SCLAccel.GPIO_PinConfig.GPIO_PinOPType						= GPIO_OTYPER_OPENDRAIN;
+	SCLAccel.GPIO_PinConfig.GPIO_PinPuPdControl					= GPIO_PUPDR_NOTHING;
+	SCLAccel.GPIO_PinConfig.GPIO_PinSpeed						= GPIO_OSPEED_FAST;
+	SCLAccel.GPIO_PinConfig.GPIO_PinAltFunMode					= AF4;
+	GPIO_Config(&SCLAccel);
+	//Configuracion de los pines para el I2C -> SDA
+	SDAAccel.pGPIOx												= GPIOB;
+	SDAAccel.GPIO_PinConfig.GPIO_PinNumber						= PIN_9;
+	SDAAccel.GPIO_PinConfig.GPIO_PinMode						= GPIO_MODE_ALTFN;
+	SDAAccel.GPIO_PinConfig.GPIO_PinOPType						= GPIO_OTYPER_OPENDRAIN;
+	SDAAccel.GPIO_PinConfig.GPIO_PinPuPdControl					= GPIO_PUPDR_NOTHING;
+	SDAAccel.GPIO_PinConfig.GPIO_PinSpeed						= GPIO_OSPEED_FAST;
+	SDAAccel.GPIO_PinConfig.GPIO_PinAltFunMode					= AF4;
+	GPIO_Config(&SDAAccel);
+	//Configuracion de la cominicacion I2C
+	handlerAccelerometer.ptrI2Cx								= I2C1;
+	handlerAccelerometer.modeI2C								= I2C_MODE_FM;
+	handlerAccelerometer.slaveAddress							= ACCEL_ADDRESS;
+	handlerAccelerometer.PLL_ON									= PLL_ENABLE;
+	i2c_Config(&handlerAccelerometer);
+}
